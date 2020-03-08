48367ef43d032e7de7d44649a8cd51981a95f9c1
Roland Angerer <dev@rangerer.at>
2020-03-08 14:18:31 +0100

    add todo item model

--- todos/api/models.py
+++ todos/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+    notes = models.TextField()
+    image = models.ImageField()
